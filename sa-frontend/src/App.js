241dc7d26cd28ca9cfbe69f7b93303b01bcca48e
RohitSquareops <rohitkumar@squareops.com>
2022-07-25 09:42:18 +0000

    added compose files

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.221.42.245:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
